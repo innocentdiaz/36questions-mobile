018933cad5d3252cc08c23548bb60a4f8ede65c3
John S <diaz.john312@gmail.com>
2018-09-14 21:40:11 -0400

    registered screens and added loginCb function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import { AsyncStorage } from 'react-native';
 import { Navigation } from 'react-native-navigation';
 import store from './redux';
-import Provider from 'react-redux';
+import { Provider } from 'react-redux';
 import { registerScreens } from './screens';
 
-
-registerScreens(store, Provider)
+registerScreens(Provider, store)
 
 const startApplication = () => {
   AsyncStorage.getItem('@TSQ_TOKEN')
@@ -32,6 +31,12 @@ const Onboarding = () => {
           topBar: {
             visible: false
           }
+        },
+        passProps: {
+          loginCb: authToken => {
+            AsyncStorage.setItem('@TSQ_TOKEN', authToken);
+            loggedInView()
+          }
         }
       },
     }
@@ -39,7 +44,17 @@ const Onboarding = () => {
 }
 
 const loggedInView = () => {
-
+  Navifation.setRoot({
+    root: {
+      stack: {
+        children: [{
+          component: {
+            name: 'app.Landing'
+          }
+        }]
+      }
+    }
+  })
 }
 
 export default startApplication
