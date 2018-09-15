544a38dc55bff5375f3e80b0e74ce1848ecd539a
John S <diaz.john312@gmail.com>
2018-09-15 11:43:34 -0400

    Added loading screen view

---
+++ src/screens/LoadingScreen.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+import Fonts from '../utils/Fonts';
+import * as Animatable from 'react-native-animatable';
+import ActivityIndicator from 'react-native-activity-indicator';
+
+class LoadingScreen extends Component {
+  render(){
+    return(
+      <View style={styles.mainContainer}>
+        <View style={{
+          justifyContent: 'space-between',
+          flex: 1
+        }}>
+          <Animatable.Text
+            animation='bounce'
+            style={styles.mainText}
+          >
+            LOADING
+          </Animatable.Text>
+
+          <ActivityIndicator
+            color='white'
+            size='large'
+          />
+        </View>
+      </View>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    backgroundColor: '#f9c296',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  mainText: {
+    color: 'white',
+    fontSize: 28,
+    fontFamily: Fonts.Playfair
+  }
+});
+
+export default LoadingScreen
