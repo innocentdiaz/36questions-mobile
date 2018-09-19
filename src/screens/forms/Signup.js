10153da25722949854331f9a1a84e611f0e8e27e
John S <diaz.john312@gmail.com>
2018-09-18 22:04:00 -0400

    Added clouds img

--- src/screens/forms/Signup.js
+++ src/screens/forms/Signup.js
@@ -1,31 +1,116 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native-elements';
+import {
+  View,
+  Image,
+  StyleSheet,
+  ScrollView
+} from 'react-native';
+import { Text, Icon } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
+import Fonts from '../../utils/Fonts';
 
 class SignUp extends Component {
-  handleMainContainerRef = ref => this.mainContainerRef = ref
-
-  componentDidMount() {
-    this.mainContainerRef.transition(
-    {
-      flex: 0,
-      opacity: 0,
-      width: 0
-    },
-    {
-      flex: 1,
-      opacity: 1
-    }, 1000)
-  }
   render(){
     return(
       <Animatable.View
-        ref={this.handleMainContainerRef}
+        animation="fadeIn"
+        style={styles.mainContainer}
       >
-        <Text h1>Signup!</Text>
+        <View
+          style={styles.topBar}
+        >
+          <Icon
+            name="chevron-left"
+            size={48}
+            color='white'
+          />
+        </View>
+        <View
+          style={styles.head}
+        >
+          <Text
+            style={{
+              ...styles.mainText,
+              fontSize: 48
+            }}
+          >
+            36
+          </Text>
+
+        </View>
+        <Animatable.View
+          style={styles.body}
+          animation='slideInUp'
+        >
+          <Image
+            source={require('../../images/clouds.png')}
+            style={{
+              width: '100%',
+              height: 60,
+              resizeMode: 'cover'
+            }}
+            />
+          <View
+            style={styles.form}
+          >
+            <Text
+              style={{fontSize: 25}}
+            >
+              TESTINGTESTINGTESTING
+              TESTINGTESTINGTESTING
+              TESTINGTESTINGTESTING
+              TESTINGTESTINGTESTING
+              TESTINGTESTINGTESTING
+              TESTINGTESTINGTESTING
+              TESTINGTESTINGTESTING
+              TESTINGTESTINGTESTING
+              TESTINGTESTINGTESTING
+              TESTINGTESTINGTESTING
+              TESTINGTESTINGTESTING
+              TESTINGTESTINGTESTING
+            </Text>
+          </View>
+
+        </Animatable.View>
       </Animatable.View>
     );
   }
 };
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    alignItems: 'stretch',
+    paddingTop: 5,
+    backgroundColor: '#f56f68'
+  },
+  mainText: {
+    color: 'white',
+    fontFamily: Fonts.Playfair
+  },
+  topBar: {
+    padding: 10,
+    alignItems: 'flex-start',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0
+  },
+  head: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    height: '45%',
+    left: 0,
+    right: 0
+  },
+  body: {
+    flex: 1
+  },
+  form: {
+    backgroundColor: 'white',
+    flex: 1
+  }
+});
 
 export default SignUp
